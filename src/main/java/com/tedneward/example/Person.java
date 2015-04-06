69912cd991904bd3dad99aedf7e569be137b08a4
TheThirdDave <uski2011@gmail.com>
2015-04-06 15:21:16 -0700

    added getter and setter methods for all the variables for Person

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -23,14 +23,46 @@ public class Person {
   public int getAge() {
     return age;
   }
+
+  public void setAge(int value) {
+    if (value < 0) {
+      throw new IllegalArgumentException("The age of a person needs to be greater than 0.");
+    } else {
+      int old = age;
+      age = value;
+
+      this.pcs.firePropertyChange("age", old, value);
+      propertyChangeFired = true;
+    }
+  }
   
   public String getName() {
     return name;
+  }
+
+  public void setname(String value) {
+    if (value == null) {
+      throw new IllegalArgumentException("The name of a person needs to not be a null string, must cotain characters.");
+    } else {
+      int old = name;
+      name = value;
+
+      this.pcs.firePropertyChange("name", old, value);
+      propertyChangeFired = true;
+    }
   }
   
   public double getSalary() {
     return salary;
   }
+
+  public void setSalary(double value) {
+    int old = age;
+    age = value;
+
+    this.pcs.firePropertyChange("salary", old, value);
+    propertyChangeFired = true;
+}
   
   public String getSSN() {
     return ssn;
